66033f34541bee50b8020faca8243a0ee7be365e
Celeste789 <celestegp97@gmail.com>
2025-01-21 23:37:02 -0300

    .

--- Popup Battle.py
+++ Popup Battle.py
@@ -59,11 +59,19 @@ class PickAPokemonScreen(tk.Frame):
         lbl_trainer1 = tk.Label(self, text="This are trainer1's pokemons: ")
         lbl_trainer1.pack()
         
-        v = tk.StringVar()
+        #v = tk.StringVar()
         trainer_team = trainer1.trainer_team
 
+        # for name, pokemon in trainer_team.items():
+        #     btn_pokemon = tk.Button(self, text=name, command=lambda p=pokemon, controller=controller, frame=PickAMoveScreen(selected_pokemon=pokemon, controller=controller): self.select_pokemon(controller, frame, p))
+        #     btn_pokemon.pack()
+        
         for name, pokemon in trainer_team.items():
-            btn_pokemon = tk.Button(self, text=name, command=lambda p=pokemon: self.select_pokemon(controller, p))
+            btn_pokemon = tk.Button(
+                self,
+                text=name,
+                command=lambda p=pokemon: self.select_pokemon(controller, p)
+            )
             btn_pokemon.pack()
                   
 
@@ -74,11 +82,14 @@ class PickAPokemonScreen(tk.Frame):
         btn_quit = tk.Button(self, text="Quit", command=screen.destroy)
         btn_quit.pack()
         
+    # def select_pokemon(self, controller, p):
+    #     controller.selected_pokemon = p
+    #     controller.change(self, frame) 
+
     def select_pokemon(self, controller, pokemon):
         controller.selected_pokemon = pokemon
-        controller.change(PickAMoveScreen(selected_pokemon=pokemon, controller=controller)) 
+        controller.change(PickAMoveScreen)        
 
-        
 class PickAMoveScreen(tk.Frame):
     def __init__(self, controller, selected_pokemon):
         super().__init__(controller.master)
@@ -87,23 +98,15 @@ class PickAMoveScreen(tk.Frame):
         lbl = tk.Label(self, text=f"Now, pick a move for {selected_pokemon.pokemon_name}!")
         lbl.pack()
         
-        v = tk.StringVar()
-        dict_moves = {}
-        
-        for move in selected_pokemon.pokemon_moves:
-            tuple_name_move = (move.move_name, move)
-            dict_moves.update(tuple_name_move)
+        dict_moves = {move.move_name: move for move in selected_pokemon.pokemon_moves}
             
-        for name, move in dict_moves,items():
-            btn_move = tk.Button(self, text=name, command=lambda m=move: self.select_move(controller, m))
+        for name, move in dict_moves.items():
+            btn_move = tk.Button(
+                self,
+                text=name,
+                command=lambda m=move: self.select_move(controller, m)
+            )
             btn_move.pack()
-                    
-            # btn_pokemon = tk.Button(self, text=name, command=lambda p=pokemon: self.select_pokemon(controller, p))
-            # btn_pokemon.pack()
-
-        # Add a button to go back to the WelcomeScreen 
-        btn = tk.Button(self, text="Back", command=lambda: controller.change(PickAPokemonScreen))
-        btn.pack()
         
         btn_quit = tk.Button(self, text="Quit", command=screen.destroy)
         btn_quit.pack()
